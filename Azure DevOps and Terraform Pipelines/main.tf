ee52bce5919d5a1021a7fbc4de3661df9f1cf8e2
saniamueza <115404818+saniamueza@users.noreply.github.com>
2022-10-09 21:55:40 +0200

    modified to run

--- Azure DevOps and Terraform Pipelines/main.tf
+++ Azure DevOps and Terraform Pipelines/main.tf
@@ -17,17 +17,17 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "vnet_rg" {
-  name     = var.resourcegroup_name
-  location = var.location
-  tags     = var.tags
+
+data "azurerm_resource_group" "vnet_rg" {
+  name = var.resourcegroup_name
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
+  location            = data.azurerm_resource_group.vnet_rg.location
+  resource_group_name = data.azurerm_resource_group.vnet_rg.name
   tags                = var.tags
 }
 
@@ -39,24 +39,4 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = each.value["address_prefixes"]
 }
 
-resource "azurerm_public_ip" "bastion_pubip" {
-  name                = "${var.bastionhost_name}PubIP"
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
 
-resource "azurerm_bastion_host" "bastion" {
-  name                = var.bastionhost_name
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  tags                = var.tags
-
-  ip_configuration {
-    name                 = "bastion_config"
-    subnet_id            = azurerm_subnet.subnet["bastion_subnet"].id
-    public_ip_address_id = azurerm_public_ip.bastion_pubip.id
-  }
-}
